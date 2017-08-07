07dd26bf84f9118ede5bb31c93567ffb15aece45
Tyson Hester <ironman9967@gmail.com>
2017-08-07 01:50:16 -0400

    Update prep.sh

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -9,12 +9,12 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 echo 'cloning iron-iot-cloud'
 git clone git@github.com:ironman9967/iron-iot-hub.git temp
 
-echo 'starting nvm'
-source ./common/scripts/start-nvm.sh
-
 echo 'getting node version from cloud'
 nodeVersion=`wget -qO- http://iron-iot-cloud:9967/api/code/versions/node | grep -o ':".*' | grep -o '[^:"} ]*'`
 echo "node version is $nodeVersion"
+
+echo 'starting nvm'
+source ./common/scripts/start-nvm.sh
 
 echo "installing node $nodeVersion"
 nvm install $nodeVersion
@@ -25,27 +25,29 @@ nvm alias default $nodeVersion
 echo "setting nvm to use $nodeVersion"
 nvm use $nodeVersion
 
-echo 'navigating to temp'
-cd temp
+ls
 
-echo 'npm install all deps'
-npm i
+# echo 'navigating to temp'
+# cd temp
 
-echo 'building app'
-npm run build
+# echo 'npm install all deps'
+# npm i
 
-echo 'navigate up from temp'
-cd ..
+# echo 'building app'
+# npm run build
 
-echo 'copying dist'
-cp -r ./temp/dist ./
-echo 'copying package.json'
-cp ./temp/package.json ./
-echo 'copying package-lock.json'
-cp ./temp/package-lock.json ./
+# echo 'navigate up from temp'
+# cd ..
 
-echo 'removing temp'
-rm -rf temp
+# echo 'copying dist'
+# cp -r ./temp/dist ./
+# echo 'copying package.json'
+# cp ./temp/package.json ./
+# echo 'copying package-lock.json'
+# cp ./temp/package-lock.json ./
 
-echo 'npm install production deps'
-npm i --production
+# echo 'removing temp'
+# rm -rf temp
+
+# echo 'npm install production deps'
+# npm i --production
