0293cc0dd412985627be4f6ed6c074cd8da87d7f
Tyson Hester <ironman9967@gmail.com>
2017-08-07 00:13:20 -0400

    Update prep.sh

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -12,36 +12,36 @@ git clone git@github.com:ironman9967/iron-iot-hub.git temp
 echo 'starting nvm'
 source ./common/scripts/start-nvm.sh
 
-echo 'installing node stable'
-nvm install stable
+# echo 'installing node stable'
+# nvm install stable
 
-echo 'setting nvm default to stable'
-nvm alias default stable
+# echo 'setting nvm default to stable'
+# nvm alias default stable
 
-echo 'setting nvm to use stable'
-nvm use stable
+# echo 'setting nvm to use stable'
+# nvm use stable
 
-echo 'navigating to temp'
-cd temp
+# echo 'navigating to temp'
+# cd temp
 
-echo 'npm install all deps'
-npm i
+# echo 'npm install all deps'
+# npm i
 
-echo 'building app'
-npm run build
+# echo 'building app'
+# npm run build
 
-echo 'navigate up from temp'
-cd ..
+# echo 'navigate up from temp'
+# cd ..
 
-echo 'copying dist'
-cp -r ./temp/dist ./
-echo 'copying package.json'
-cp ./temp/package.json ./
-echo 'copying package-lock.json'
-cp ./temp/package-lock.json ./
+# echo 'copying dist'
+# cp -r ./temp/dist ./
+# echo 'copying package.json'
+# cp ./temp/package.json ./
+# echo 'copying package-lock.json'
+# cp ./temp/package-lock.json ./
 
-echo 'removing temp'
-rm -rf temp
+# echo 'removing temp'
+# rm -rf temp
 
-echo 'npm install production deps'
-npm i --production
+# echo 'npm install production deps'
+# npm i --production
